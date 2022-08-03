95d9a9dd123bc114666aedd48ded3d6bd4e9db35
AdyC24 <adychandra@gmail.com>
2022-08-03 16:58:08 +0800

    5 lagi js

---
+++ javascript/8kyu/Beginner Series #4 Cockroach/index.js
@@ -0,0 +1,38 @@
+/*
+------------ INSTRUCTION ----------
+The cockroach is one of the fastest insects. Write a function which takes its speed in km per hour and returns it in cm per second, rounded down to the integer (= floored).
+
+For example:
+
+1.08 --> 30
+Note! The input is a Real number (actual type is language dependent) and is >= 0. The result should be an Integer.
+
+
+----------- SAMPLE TASKS ------------
+const chai = require("chai");
+const assert = chai.assert;
+chai.config.truncateThreshold=0;
+
+describe("Basic Tests", function() {
+  it("Testing for fixed tests", () => {
+    assert.strictEqual(cockroachSpeed(1.08), 30);
+    assert.strictEqual(cockroachSpeed(1.09), 30);
+    assert.strictEqual(cockroachSpeed(0), 0);
+  });
+});
+
+
+------------------
+INTISARI INSTRUKSI
+
+
+*/
+
+function cockroachSpeed(s) {
+    return Math.floor(s * 27.77777778)
+  }
+
+// Itteration
+ const cockroachSpeed = (s) => Math.floor(s * 27.77777778)
+
+ 
---
+++ javascript/8kyu/Find Nearest square number/index.js
@@ -0,0 +1,42 @@
+/*
+------------ INSTRUCTION ----------
+Your task is to find the nearest square number, nearest_sq(n), of a positive integer n.
+
+Goodluck :)
+
+Check my other katas:
+
+Alphabetically ordered
+
+Case-sensitive!
+
+Not prime numbers
+
+
+----------- SAMPLE TASKS ------------
+describe("Basic tests",() => {
+  Test.assertEquals(nearestSq(1),1)
+  Test.assertEquals(nearestSq(2),1)
+  Test.assertEquals(nearestSq(10),9)
+  Test.assertEquals(nearestSq(111),121)
+  Test.assertEquals(nearestSq(9999),10000)
+  
+});
+
+------------------
+INTISARI INSTRUKSI
+
+
+*/
+
+function nearestSq(n){
+    return Math.round(Math.sqrt(s)) ** 2
+}
+
+function nearestSq(n){
+    return Math.pow(Math.round(Math.sqrt(n)), 2)
+}
+
+// Itteration
+
+const nearestSq = (n) => Math.pow(Math.round(Math.sqrt(n)), 2)
